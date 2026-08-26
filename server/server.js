5d2c8f9da8292c0ca82f4ac76b6b3dec99c7fe2f
akhil <b.akhil667@gmail.com>
2026-08-26 19:43:18 +0530

    first commit

--- server/server.js
+++ server/server.js
@@ -11,19 +11,22 @@ const bookingRoutes = require('./routes/bookings');
 
 const app = express();
 
-// Middleware
+
 app.use(cors());
 app.use(express.json());
 
-// Routes
+app.get("/",(req,res)=>{
+  return res.status(200).json({message:"Eventora API Running succesfully"})
+})
+
 app.use('/api/auth', authRoutes);
 app.use('/api/events', eventRoutes);
 app.use('/api/bookings', bookingRoutes);
 
-// Database Connection
+
 mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/eventora')
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.error('MongoDB Connection Error:', err));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
